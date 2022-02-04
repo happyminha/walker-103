4d580cb932d91dda8a8eeef2069df5212f046934
THE MINHA <85743303+happyminha@users.noreply.github.com>
2022-02-04 23:10:25 +0900

    Update and rename 103.sql to walker 103.sql

--- walker 103.sql
+++ walker 103.sql
@@ -1,32 +1,23 @@
-1.
-/*
- * 총 내원일수 구하기
- */
-SELECT SUM (visit_date) FROM (SELECT (visit_end_date - visit_start_date +1 ) --'방문종료일-방문시작일+1'을 총 내원일수로 지정
- AS visit_date FROM visit_occurrence) A;
-
-
-
+1. 총내원일수, 최댓값 최솟값을 구하기 위해 visit_end_date - visit_start_date +1 조건을 수행한 사전 테이블 만든 이후 수행
 /*
  * 총 내원일수의 최댓값과 최솟값 구하기
  */
-SELECT MAX(visit_date), MIN(visit_date) FROM  --총 내원일수의 최댓값과 최솟값
+SELECT SUM(visit_date), MAX(visit_date), MIN(visit_date) FROM  --총 내원일수의 최댓값과 최솟값
 (SELECT (visit_end_date - visit_start_date +1) AS visit_date FROM visit_occurrence) A;
 
 
-2.
-
+2. UPPER를 사용하여 대소문자 구분없이 데이터를 추출하고 추출한 데이터에서 heart 문자열이 있는 조건을 추가하여 추출
 select distinct A.concept_name --중복비허용 
 from concept A inner join condition_occurrence B 
 ON A.concept_id = B.condition_concept_id where upper(A.concept_name) ~ '^A|^B|^C|^D|^E' and A.concept_name like '%heart%'; --A~E 및 heart포함
 
-3.
+3. 복용 종요일과 복용 시작일의 차이를 구해 순서 정렬
 SELECT *, (drug_exposure_end_date - drug_exposure_start_date) AS drug_exposure_date --종료일 -시작일을 복용일로 지정
 FROM drug_exposure
 WHERE person_id = '1891866'
 ORDER BY drug_exposure_date DESC;  --order by로 복용일 내림차순 정렬
 
-4. 
+4. 사전에 만들어진 조건인 drug_list, drugs, prescription_count를 사용하여 짝지어진 처방건수의 drug_concept_id1, drug_concept_id2의 count를 구하고 테이블을 조인하여 요구하는 조건 추출 
 with drug_list as ( 
  select distinct drug_concept_id, concept_name, count(*) as cnt from drug_exposure de 
  join concept 
@@ -44,7 +35,7 @@ from drug_pair_cnt1 A inner join drug_pair_cnt2 B on (A.drug_concept_id1 = B.dru
  inner join drug_list C on A.drug_concept_id1  = C.drug_concept_id
  order by cnt1
 
-5. 
+5. 제 2형 당뇨 진단받은 환자수 테이블을 구하고 18세이상, metformin 테이블을 순차적 구하고 각각의 테이블을 조인이후 진단이후(진단일이 없는 데이터 제외) 90일 이상 복용 조건 추출
 with diabetes as (
 select * from condition_occurrence where condition_concept_id in ('3191208','36684827','3194332','3193274','43531010','4130162',
 '45766052', '45757474','4099651','4129519','4063043','4230254','4193704','4304377','201826','3194082','3192767') --제 2형 당뇨 진단 받은 환자 수 (personid로 count함)
@@ -59,9 +50,12 @@ select *
 from diabetes A inner join birth B on A.person_id = B.person_id inner join metformin C on  A.person_id = C.person_id
 where drug_exposure_end_date - drug_exposure_start_date >= 90 and condition_start_date is not null; 
 
-6.
+6.각각의 패턴을 문자열로 만든후 나열하고 정규식을 사용하여 패턴을 단순화 시킨이후 group by를 사용하여 cnt 값 추출
+-- 패턴 고정
+select regexp_replace('aaaaabbbbccdaa',  '(.)\1*' ,'\1', 'g') 
 
-
-7. 
+7. sql을 사용하여 사전 추출 테이블을 생성하고 python 코드를 사용하여 조건에 맞는 테이블 추출
 -- CONTINUING 데이터 제거 후 사전 추출 테이블 생성
 CREATE TABLE clinical_note_regex AS select regexp_replace(note, 'CONTINUING.*', '', 'g') from clinical_note;
+
+--이후는 python 코드
